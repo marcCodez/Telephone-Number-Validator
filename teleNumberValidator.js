e8b72af19b9c76a642adeac485576354088c9fa0
marcCodez <marc_pulumbarit@hotmail.com>
2020-05-05 03:53:27 +1000

    Progress pt 2

--- teleNumberValidator.js
+++ teleNumberValidator.js
@@ -7,21 +7,49 @@ function telephoneCheck(str) {
 	let elevenDigits = /^[1-9]{1,11}$/g;
 	let tenDigits = /^[1-9]{1,10}$/g;
 	let correctSymbols = /\s|\-/g;
-	let correctBrackets = /\(\w+\)/g;
+	let correctBrackets = /\(\w\w\w\)/g;
+	let spaces = /\s/g;
+	let numbers = /\w/g;
+
+	console.log(str.match(numbers).length);
 
 	let testS = str.replace(correctSymbols, "");
-	console.log(testS);
-	// If starts with 1
-	if (
-		correctBrackets.test(testS) === true &&
-		startsWithOne.test(testS) === true
+	let testSS = testS.replace(correctBrackets, "");
+
+	console.log(correctBrackets.test(str));
+	//
+
+	if (str.match(numbers).length !== 10 && str.match(numbers).length !== 11) {
+		return false;
+	} else if (
+		str.match(numbers).length === 11 &&
+		startsWithOne.test(str) === false
 	) {
-		return true;
-	} // else if (){
+		return false;
+	}
+	// If starts with 1
+	// if (correctBrackets.test(str) === true && startsWithOne.test(str) === true && str.match(numbers).length <= 11){
+	//   return true
+	// } else if (startsWithOne.test(str) === true && str.match(numbers).length === 11 && correctBrackets.test(str) === true ){
+	//   return true
+	// }else if (correctBrackets.test(str) === true && str.match(numbers).length === 10 ) {
+	//   return true
+	//   } else if (startsWithOne.test(str) === true && str.match(numbers).length === 11) {
+	//     return true
+	//   } else if ()
 
+	// } else if (str.match(numbers).length === 10) {
+	//   return true
 	// }
+	// } else if (startsWithOne.test(testS) === true && elevenDigits.test(testS) === true){
+	// return true
+	//  } else if (testS.length === 10) {
+	//    return true
+	//  } else if (testSS.length === 7 || correctBrackets.test(testS) === true) {
+	// return true
+	//   }
 
-	return false;
+	return true;
 }
 
 console.log(telephoneCheck("1 555-555-5555"));
